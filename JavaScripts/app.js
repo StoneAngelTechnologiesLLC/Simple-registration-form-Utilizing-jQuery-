1ba9f818ff6eb9ccec19df1cf580193b8ef5fd9d
Pietrangelo <stoneangeltechnologies@gmail.com>
2017-02-25 17:56:48 -0700

    final version complete

--- JavaScripts/app.js
+++ JavaScripts/app.js
@@ -4,135 +4,177 @@
                                     
 //			If opening browser off-line, jQuery code will not be recognized by your computer!
 
-
-// *** Uncomment the lines 8 thru 12 as well as line 62, 85 & 86 below to begin Phases 1-6b ***	
-//  Uncomment each Phase, in order. Open HTML file in browser after uncommenting a phase to see result 
-
-// var main = 
-	
-// 	function()
-// 	{
-// 		"use strict";
-
-// 		$(".comments button").on("click",  function(event)
-												//		Uncomment for block below for Phase# 1 
-											    // {
-											   	// 		console.log("hello World!");
-											   	// }
-
-											   	   //		Uncomment for block below for Phase# 2 
-											   	   // {
-											   	   // 		$(".comments-input").append("<p>This is a new comment<p/>");
-											   	   // }
-
-											   	      //        Uncomment for block below for Phase# 3 
-											   	      // {
-											   	      // 		var $new_comment = $("<p>");
-											   	      // 		$new_comment.text("This is a new comment");
-											   	      // 		
-											   	      //        $(".comments-input").append($new_comment);
-											   	      // }
-
-											   	         //		Uncomment for block below for Phase# 4
-											   	         // {
-											   	         // 	var $new_comment = $("<p>").text("This is a new comment");
-											   	         // 	$(".comments-input").append($new_comment);
-											   	         // }
-
-											   	    //	Uncomment for block below for Phase# 5 	   
-											   		// {
-											   		// 	var $new_comment = $("<p>"),
-											   			
-											   		// 	comment_text = $(".comments input").val();
-
-											   		// 	$new_comment.text(comment_text);
-
-											   		// 	$(".comments-input").append($new_comment);
-											   		// }
-
-
-											   		//  Uncomment for block below for Phase# 6-a
-		      //                                       {
-								// 			   			var $new_comment;
-											 
-								// 			   			if ($(".comments input").val() !== "") 
-								// 			   			{
-								// 			   				$new_comment = $("<p>").text($(".comments input").val());
-								// 			   				$new_comment.hide();
-								// 			   				$(".comments-input").append($new_comment);
-								// 			   				//Defualt parameter value is 400/ms: 
-								// 							//Either value, number (500) or string ("500") data-type will be accepted.
-								// 							$new_comment.fadeIn(1000);
-								// 			   				$(".comments input").val("");
-								// 			   			}	
-								// 	
-
-				// *** Uncomment the line below for Phases 1-6b ***				   		}
-								// );
-
-	//	   Uncomment for block below for Phase# 6-b
-		// $(".comments input").on("keypress", function (event) 
-		// {
-		// 	var $new_comment;
-
-		// 	if (event.keyCode === 13)
-		// 	{
-		// 		if ($(".comments input").val() !== "") 
-		// 		{
-		// 			$new_comment = $("<p>").text($(".comments input").val());
-		// 			$new_comment.hide();
-		// 			$(".comments-input").append($new_comment);
-		// 			//Defualt parameter value is 400/ms: 
-		// 			//Either value, number (500) or string ("500") data-type will be accepted.
-		// 			$new_comment.fadeIn(1000);
-		// 			$(".comments input").val("");
-		// 		}	
-
-		// 	}
-		// });
-     // *** Uncomment the 2 lines below for Phases 1-6b ***	       
- //    };      																								
-	// $(document).ready(main);
-
-
-	// 						***	FINAL Phase# 7 ***
-	// Comment out the code below before beginning the instructions 
-	// at begining of this file, to fire Phase# 1 correctly.
-
-
-
 var main = function()
 {
 	"use strict";
+	//										** Phase #5
 
-	var addCommentFromInputBox = function()
+	var toDos = [
+					"Get Groceries",
+					"Make up some new ToDos",
+					"Prep for Monday's class",
+					"Checkout Linked-in",
+					"Take Gracie to the park",
+					"Finish writing this book"
+				];
+
+	$(".tabs a span").toArray().forEach(function(element)
 	{
-		var $new_comment;
+		var $element = $(element);
 
-		if ($(".comments input").val() !== "") 
+		// To create an event("click") handler for this element
+		$(element).on("click", function()
+		{
+			//since we are using jQuery version of element,
+			//We will create temporary variable
+			//so we don't have to re-create it.
+			var $content, $input, $button;
+
+			$(".tabs a span").removeClass("active");
+			$element.addClass("active");
+			$("main .content").empty();
+
+			if ($element.parent().is(":nth-child(1)"))
 			{
-		 		$new_comment = $("<p>").text($(".comments input").val());
-		 		$new_comment.hide();
-		 		$(".comments-input").append($new_comment);
-		 		//				fadeIn function's Defualt parameter value is 400/ms: 
-		 		//		Either value: number (500) or string ("500") data-type will be accepted.
-		 		$new_comment.fadeIn(1000);
-		 		$(".comments input").val("");
-		 	}	
+				$(".mainContainer").css("width", "66.66%");
+				$content = $("<ul>");
+				// for (var i = 0; i <= toDos.length; i++) // 1st to last
+				for (var i = toDos.length-1; i >= 0; i--)  // last to 1st
+				{
+					$content.append($("<li>").text(toDos[i]));
+				}
+				//$("main .content").append($content);
+				//$content.append($("<li>").text("hello"));
+			}
+
+			else if ($element.parent().is(":nth-child(2)"))
+			{
+				$(".mainContainer").css("width", "66.66%");
+				$content = $("<ul>");
+				toDos.forEach(function (todo)
+				{
+					$content.append($("<li>").text(todo));
+				});
+			} 
+			else if ($element.parent().is(":nth-child(3)"))
+			{
+				$(".mainContainer").css("width", "33.2%");
+				$input = $("<input>");
+
+				$button = $("<button>").text("ASU");
+
+				$button.on("click", function()
+				{
+					if($input.val() !== "")
+					{
+						toDos.push($input.val());
+						$input.val("");
+					}
+				});
+
+				$content = $("<div>").append($input, $button);
+			} 
+ 			
+ 			$("main .content").append($content);
+			return false;
+		});
+	});	
+
+	$(".tabs a:first-child span").trigger("click");
 	};
 
-	$(".comments button").on("click",  function(event)
-	{
-		addCommentFromInputBox();
-	});
-
-	$(".comments input").on("keypress", function (event)
-	{
-		if (event.keyCode === 13)
-		{ 
-			addCommentFromInputBox();
-		}
-	});
-};
-
 $(document).ready(main);
+
+
+
+	//$(".tabs a:first-child span").trigger("click");
+	// //									** Phase #4 Using A ForEach Loop, Array
+
+	// 	$(".tabs a span").toArray().forEach(function (element)
+	// 	{
+	// 		// To create an event("click") handler for this element
+	// 		$(element).on("click", function()
+	// 		{
+	// 			$("tabs. a span").removeClass("active");
+	// 			$(element).addClass("active");
+	// 			$("main .content").empty();
+	// 			return false;
+	// 		}); 
+
+	// 	});
+ // 	//  //								** Phase #3 Using A For Loop
+
+	// //  var tabnumber;
+	// //  for (tabNumber = 1; tabnumber <= 3; tabnumber++) 
+	// //  {
+	// //  	var tabSelector = ".tabs a:nth-child("+ tabNumber +") span";
+	// //  }
+
+	// //  $(tabSelector).on("click", function() 
+	// //  {
+	// //  	$(".tabs span").removeClass("active");
+	// //  	$(tabSelector).addClass("active");
+	// //  	return false;
+	// //  })
+
+ // 	// //  	//  						** Phase #2 Refactoring Using A Funstion
+	
+	// // // var makeTabActive = function(tabNumber)
+	// // // {
+	// // // 	// construct the selector from the tab number
+	// // // 	var tabSelector = ".tabs a:nth-child("+ tabNumber +") span";
+	// // // 	$(".tabs span").removeClass("active");
+	// // // 	$(tabSelector).addClass("active");
+	// // // }
+
+	// // // //  						** Phase #2 Refactoring Using A Funstion
+
+	// // // $(".tabs a:nth-child(1)").on("click", function ()
+	// // // {
+	// // // 	makeTabActive(1);
+		
+	// // // 	// //  					** Phase #1 Creating The Tab Functonality
+
+	// // // 	// // To make ALL tabs INACTIVE.
+	// // // 	// $(".tabs span").removeClass("active");
+
+	// // // 	// // To make 1st tab ACTIVE
+	// // // 	// $(".tabs a:nth-child(1) span").addClass("active");
+
+	// // // 	// // To empty main section, in order to re-create it
+	// // // 	// $("main .content").empty();
+
+	// // // 	// To return false, so we don't follow the link
+	// // // 	return false;
+	// // // });
+
+	// // // //  						** Phase #2 Refactoring Using A Funstion
+
+	// // // $(".tabs a:nth-child(2)").on("click", function()
+	// // // {
+	// // // 	makeTabActive(2);
+	// // // 	// //  					** Phase #1 Creating The Tab Functonality
+
+	// // // 	// $(".tabs span").removeClass("active");
+	// // // 	// $(".tabs a:nth-child(2) span").addClass("active");
+	// // // 	// $("main .content").empty();
+	// // // 	return false;
+	// // // });
+
+	// // // //  						** Phase #2 Refactoring Using A Funstion
+
+	// // // $(".tabs a:nth-child(3)").on("click", function()
+	// // // {
+	// // // 	makeTabActive(3);
+	// // // 	// //  					** Phase #1 Creating The Tab Functonality
+
+	// // // 	// $(".tabs span").removeClass("active");
+	// // // 	// $(".tabs a:nth-child(3) span").addClass("active");
+	// // // 	// $("main .content").empty();
+	// // // 	return false;
+	// // // });
+
+
+// };
+
+// $(document).ready(main);
